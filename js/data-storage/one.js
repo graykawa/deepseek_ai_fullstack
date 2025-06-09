40a28364f878ae3fdb161bf3d5eb097836adcc24
graykawa <2975976208@qq.com>
2025-06-10 01:36:52 +0800

    the data storage strategy of javascript

---
+++ js/data-storage/one.js
@@ -0,0 +1,13 @@
+let str = 'hello'
+let num = 123
+let f = true
+let nu = null
+let un = undefined
+// 以上都是字面量
+
+// 以下都是构造函数的创建方式
+let str1 = new String('hello')
+let num1 = new Number(123)
+let f1 = new Boolean(true)
+let nu1 = new Null()
+let sy = Symbol('hello')
---
+++ js/data-storage/two.js
@@ -0,0 +1,17 @@
+let obj = {
+    name: '梅兰妮',
+    age: 18,
+    health: 100,
+    smoking: function() {
+        console.log('抽')
+        obj.health--
+    },
+    drinking: function() {
+        console.log('喝')
+        obj.health++
+    }
+}
+let girlName = 'myGirl'
+obj[girl] = 'Molly'
+delete obj.myGirl
+
